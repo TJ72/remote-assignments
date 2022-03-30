369b5cd34c7f0119bc217b7e4adee72bdebd7609
TJ72 <62875636+TJ72@users.noreply.github.com>
2022-03-30 21:06:20 +0800

    Add files via upload

--- week-2/Assignment 3.js
+++ week-2/Assignment 3.js
@@ -1,7 +1,7 @@
 // Function, Array, and Object
 
 function avg(data){
-    let size = data.size;
+    const size = data.size;
     const products = data.products;
     let totalCost = 0;
     for (let i = 0; i < products.length; i++) {
